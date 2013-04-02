c279703b5ba0394fc97caebb363ff61f0545ecd4
electrodynatronic <electrodynatronic@gmail.com>
2013-04-02 12:41:06 -0500

    Update CosmTestSerialAnswerer2.ino
    Updated and corrected some of the comments -CN

--- CosmTestSerialAnswerer2/CosmTestSerialAnswerer2.ino
+++ CosmTestSerialAnswerer2/CosmTestSerialAnswerer2.ino
@@ -6,28 +6,28 @@ Waits for incoming serial one-byte messages, reads & parses them, and sends an a
 
  
  The circuit:
- LM35 connected to Analog In 3
- Photocell + Resistor 3.3k connected to Analog In 5
- * potentiometer connected to analog pin 0.
-   Center pin of the potentiometer goes to the analog pin.
-   side pins of the potentiometer go to +5V and ground
- * LED connected from digital pin 9 to ground
- 
+ LM35 output is connected to Analog In 3
+ Phototransistor circuit output is connected to Analog In 0
+ Potentiometer wiper connected to analog pin 1.
+ Center pin of the potentiometer goes to the analog pin.
+ side pins of the potentiometer go to +5V and ground
+// Photocell + Resistor 3.3k connected to Analog In 5  NOTE, no photocells are available 
  
  This example code is in the public domain.
  
  */
 
 // These constants won't change.  They're used to give names
-// to the pins used:
+// to the pins used: 
 const int analogLightInput = A0;  // Analog input pin that the phototransistor is attached to
 const int analogPotInput = A1;    // Analog input pin that the potentiometer is attached to
-const int analogTempInput = A3; // Analog output pin that the LED is attached to
+const int analogTempInput = A3; // Analog output pin that the LM35 is attached to
 
 int LightSensorValue = 0;        // value read from the pot
 int TempSensorValue = 0;        // value output to the PWM (analog out)
 char command='0';
-boolean ticker=0;
+boolean ticker=0;               //Holds state for our indicator LED
+
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600); 
